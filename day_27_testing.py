46daf030d98e9978266f52ec0e8719fe356f5ad4
chanshik <chanshik@gmail.com>
2017-04-24 13:33:24 +0900

    Add Day 27: Testing

---
+++ day_27_testing.py
@@ -0,0 +1,17 @@
+"""
+Day 27: Testing 
+
+https://www.hackerrank.com/challenges/30-testing
+"""
+if __name__ == '__main__':
+    print("5")
+    print("4 3")
+    print("-1 3 0 4")
+    print("6 2")
+    print("0 -1 2 1 4 6")
+    print("7 5")
+    print("-1 0 1 2 3 4 5")
+    print("8 5")
+    print("-5 -4 -3 -2 -1 0 1 2")
+    print("9 3")
+    print("-1 0 1 2 3 4 5 6 7")
